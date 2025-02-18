cf788a5beef7d83bafd42da55714f484d2af2441
benish <benishnisarkhan56@gmail.com>
2025-02-18 12:37:00 -0800

    work updated

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -35,7 +35,7 @@
     </div>
 
  <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
     <div class="container-fluid">
         <!-- Location Button attached to the left -->
         <button style="width:200px;height:50px; border-radius:none;background-color:#f4335b;color:white;font-size:20px;" class="btn  dropdown-toggle me-3" id="locationDropdown" data-bs-toggle="dropdown" aria-expanded="false">
@@ -198,22 +198,398 @@
 <h1 class="text-center">The Best Services For You
 </h1>
 <div class="row">
-<div class="col-md-4 col-lg-4">
 
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="col-md-4 col-lg-4">
+        <div class="card custom-card" style="width: 18rem; height: 500px;">
+            <img src="{{ asset('assets/img/Cctv-Install-1.jpg') }}" class="card-img-top" alt="..." style="height: 250px;">
+            <div class="card-body text-center">
+                <h5 class="card-title">CCTV Camera Installation</h5>
+                <p class="card-text">
+                    Are you thinking about enhancing security for your home or business?
+                </p>
+                <a href="#" class="btn btn-dark text-light">Read More</a>
+            </div>
         </div>
-      </div>
+    </div>
+
+
+{{-- two --}}
+<div class="col-md-4 col-lg-4">
+    <div class="card custom-card" style="width: 18rem; height: 500px;">
+        <img src="{{ asset('assets/img/tv-antenna-install-1.jpg') }}" class="card-img-top" alt="..." style="height: 250px;">
+        <div class="card-body text-center">
+            <h5 class="card-title">
+                Digital Antennas
+                Digital Antenna Installation</h5>
+            <p class="card-text">
+                Experts in the Industry Offering Their Expertise in Installing Digital TV Antennas in Your Area.
+            </p>
+            <a href="#" class="btn btn-dark text-light">Read More</a>
+        </div>
+    </div>
+</div>
+
+
+{{--three  --}}
+<div class="col-md-4 col-lg-4">
+    <div class="card custom-card" style="width: 18rem; height: 500px;">
+        <img src="{{ asset('assets/img/Cctv-Install-1.jpg') }}" class="card-img-top" alt="..." style="height: 250px;">
+        <div class="card-body text-center">
+            <h5 class="card-title">
+                TV Wall Mounting</h5>
+            <p class="card-text">
+                Our expert technicians will help you select the best wall bracket to suit your specific requirements, ensuring a smooth and trouble-free installation.
+            </p>
+            <a href="#" class="btn btn-dark text-light">Read More</a>
+        </div>
+    </div>
+</div>
+
+
+{{-- four --}}
+
+
+<div class="col-md-4 col-lg-4 mt-5">
+    <div class="card custom-card" style="width: 18rem; height: 500px;">
+        <img src="{{ asset('assets/img/tv-antenna-install-1.jpg') }}" class="card-img-top" alt="..." style="height: 250px;">
+        <div class="card-body text-center">
+            <h5 class="card-title">CCTV Camera Installation</h5>
+            <p class="card-text">
+                Are you thinking about enhancing security for your home or business?
+            </p>
+            <a href="#" class="btn btn-dark text-light">Read More</a>
+        </div>
+    </div>
+</div>
+
+
+<div class="col-md-4 col-lg-4 mt-5">
+    <div class="card custom-card" style="width: 18rem; height: 500px;">
+        <img src="{{ asset('assets/img/Cctv-Install-1.jpg') }}" class="card-img-top" alt="..." style="height: 250px;">
+        <div class="card-body text-center">
+            <h5 class="card-title">CCTV Camera Installation</h5>
+            <p class="card-text">
+                Are you thinking about enhancing security for your home or business?
+            </p>
+            <a href="#" class="btn btn-dark text-light">Read More</a>
+        </div>
+    </div>
+</div>
+
+
+<div class="col-md-4 col-lg-4 mt-5">
+    <div class="card custom-card" style="width: 18rem; height: 500px;">
+        <img src="{{ asset('assets/img/Cctv-Install-1.jpg') }}" class="card-img-top" alt="..." style="height: 250px;">
+        <div class="card-body text-center">
+            <h5 class="card-title">CCTV Camera Installation</h5>
+            <p class="card-text">
+                Are you thinking about enhancing security for your home or business?
+            </p>
+            <a href="#" class="btn btn-dark text-light">Read More</a>
+        </div>
+    </div>
+</div>
 
 
 </div>
+{{-- row --}}
+</div>
+{{--container  --}}
+
+
+
+
+{{-- container --}}
+<div class="container">
+    <div class="row">
+        <!-- First Column (4 Images) -->
+        <div class="col-md-6 col-lg-4">
+            <div class="row">
+                <!-- Row 1 -->
+                <div class="col-6 text-center">
+                    <img src="{{ asset('assets/img/services.png') }}" class="img-fluid" width="100">
+                    <h6 class="text-center">Service on the Same Day
+                    </h6>
+                </div>
+                <div class="col-6 text-center">
+                    <img src="{{ asset('assets/img/20years.png') }}" class="img-fluid" width="100">
+                    <h6 class="text-center">20 Years Experience</h6>
+
+                </div>
+
+                <!-- Row 2 -->
+                <div class="col-6 text-center">
+                    <img src="{{ asset("assets/img/hand.png")}}" class="img-fluid" width="100">
+                    <h6 class="text-center">0 initial cost
+                    </h6>
+
+                </div>
+                <div class="col-6 text-center">
+                    <img src="{{ asset("assets/img/check.png")}}" class="img-fluid" width="100">
+                    <h6 class="text-center">Offer for Pensioner and free quote
+                    </h6>
+                </div>
+
+                <div class="col-6 text-center">
+                    <img src="{{ asset("assets/img/dollar.png")}}" class="img-fluid" width="100">
+                    <h6 class="text-center">0 initial cost
+                    </h6>
+
+                </div>
+                <div class="col-6 text-center">
+                    <img src="{{ asset("assets/img/tv.png")}}" class="img-fluid" width="100">
+                    <h6 class="text-center">Offer for Pensioner and free quote
+                    </h6>
+                </div>
+
+
+            </div>
+        </div>
+
+
+        <!-- Second Column (2 Images - One on Top, One Bottom) -->
+        <div class="col-md-6 col-lg-4 d-flex flex-column justify-content-between align-items-center">
+            <img src="{{ asset('assets/img/Front-post.jpg') }}" style="border-radius:50%; width:300px;" class="img-fluid my-2">
+            <img src="{{ asset('assets/img/by.jpg') }}" style="border-radius:50%; width:300px;position:relative;top:-70px;" class="img-fluid my-2">
+        </div>
+
+
+        <!-- Third Column (3 Images) -->
+        <div class="col-md-4 col-lg-4">
+            <div class="row">
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/one.png") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/two.jpeg") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/one.png") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/four.jpeg") }}" class="img-fluid my-2" width="100">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/five.jpg") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/six.png") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/seven.jpg") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/eight.png") }}" class="img-fluid my-2" width="100">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/one.png") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/two.jpeg") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/one.png") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/four.jpeg") }}" class="img-fluid my-2" width="100">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/five.jpg") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/six.png") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/seven.jpg") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/eight.png") }}" class="img-fluid my-2" width="100">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/one.png") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/two.jpeg") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/one.png") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/four.jpeg") }}" class="img-fluid my-2" width="100">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/five.jpg") }}" class="img-fluid my-2" width="100">
+                </div>
+                <div class="col-3">
+                    <img src="{{ asset("assets/img/six.png") }}" class="img-fluid my-2" width="100">
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </div>
 </div>
 
+
+{{-- container --}}
+
+
+
+
+
+
+{{-- container--}}
+<div class="container-fluid" style="background-image: url('assets/img/superhero-banner.png');width:100%;height:500px; background-size: cover; background-position: center; background-repeat: no-repeat; ">
+<div class="row">
+
 </div>
+</div>
+{{-- superheros --}}
+
+
+
+{{-- container--}}
+<div class="container-fluid mt-5" >
+
+<div class="row">
+<div class="col-sm-12 col-md-6 col-lg-6">
+<h2>About Our Company
+</h2>
+<p>At Best Antennas, we're the experts you can rely on for everything related to antennas. With many years of experience in installing antennas, fixing them, and mounting TVs, we've become the top choice for many happy customers.</p>
+<h6>We're here to help you out.
+</h6>
+<p>Our group, known as one of the most trusted in Australia, is dedicated to providing fast and easy solutions for any problems with your antenna.</p>
+<a href="#" class="btn text-light" style="background-color:#f4335b;">Read More</a>
+
+</div>
+
+
+    <!-- Second Column (2 Images - One on Top, One Bottom) -->
+    <div class="col-md-6 col-lg-4 d-flex flex-column justify-content-between align-items-center">
+        <img src="{{ asset('assets/img/by.jpg') }}" style=" width:300px;height:350px;" class="img-fluid my-2">
+        <img src="{{ asset('assets/img/Front-post.jpg') }}" style="width:300px;height:350px;position: relative;top:-70px;left:90px;" class="img-fluid my-2">
+
+    </div>
+
+    </div>
+    </div>
+
+
+
+
+<!-- footer -->
+<footer>
+    <div class="footer-body  mt-5" style="background-color:#161921;">
+  <div class="container-fluid">
+  <div class="row">
+  <div class="col-md-3 text-light mt-5">
+   <ul class="navbar-nav">
+
+
+    <li class="nav-item"><a href="index.html" style="text-decoration: none; color: white;">  <h5> About Our Company </h5> </a></li>
+    <li class="nav-item mt-4"><a href="about.html" style="text-decoration: none; color: white;">Best Antennas: Specialists in delivering a perfect TV signal. Our dedication lies in providing best quality, precision, and ensuring customer satisfaction.</a></li>
+    <li class="nav-item mt-3"><a href="#" style="text-decoration: none; color: white;"><i style="font-size:25px;" class="fab fa-facebook fa-lg"></i>                <i style="font-size:25px;" class="fab fa-instagram fa-lg"></i>
+
+    </a></li>
+
+   </ul>
+
+
+
+  </div>
+  <!-- col -->
+  <div class="col-md-3 text-light mt-5">
+    <ul class="navbar-nav">
+
+
+     <li class="nav-item "><h6><a href="#" style="text-decoration: none; color: white;" ><h5>Quicks Links </h5> </a></li>
+     <li class="nav-item text-light"><a href="#" style="text-decoration: none; color: white;" >Home</a></li>
+     <li class="nav-item text-light"><a href="#" style="text-decoration: none; color: white;" >About Us</a></li>
+     <li class="nav-item text-light"><a href="#" style="text-decoration: none; color: white;" >Services </a></li>
+     <li class="nav-item text-light"> <a href="#" style="text-decoration: none; color: white;" > Contact Us </a></li>
+     <li class="nav-item text-light"><a href="#" style="text-decoration: none; color: white;" > Terms and Conditions  </a> </li>
+
+    </ul>
+
+
+
+   </div>
+   <!-- col -->
+
+
+   <div class="col-md-3 text-light mt-5">
+    <ul class="navbar-nav">
+
+
+      <li class="nav-item"><a href="#" style="color: white;text-decoration: none;"><h5>Our Services</h5></a></li>
+      <li class="nav-item"><a href="#" style="text-decoration: none;color: white;">CCTV Installation</a></li>
+      <li class="nav-item"> <a href="#" style="text-decoration: none;color: white;" > Digital Antenna Installation</a></li>
+      <li class="nav-item"> <a href="#" style="text-decoration: none;color: white;" >Fextel Installation</a></li>
+      <li class="nav-item"> <a href="#" style="text-decoration: none;color: white;" > MATV & Commerical </a></li>
+     </ul>
+
+  </div>
+   <!-- col -->
+   <div class="col-md-3 text-light mt-5">
+    <ul class="navbar-nav">
+    <li class="nav-item"><a href="#" style="color: white;text-decoration: none;"><h5>Contact Us</h5></a></li>
+      <li class="nav-item"><a href="#" style="text-decoration: none;color: white;">   03070501946 </a> </li>
+      <li class="nav-item"> <a href="#" style="text-decoration: none;color: white;" >
+        info@bestantennas.com.au</a></li>
+      <li class="nav-item"> <a href="#" style="text-decoration: none;color: white;">
+        Australia </a></li>
+
+
+
+     </ul>
+
+  </div>
+   <!-- col -->
+  <hr class="mt-3" style="border: 2px solid red;">
+  <div class="col-md-12 text-light">
+    <div class="d-flex align-items-center justify-content-between">
+        <img src="{{ asset('assets/img/Logos.png') }}" height="80px">
+        <h5 class="mb-0">Copy designed By Benish Nisar Khan</h5>
+    </div>
+</div>
+
+
+
+   <!-- col -->
+
+
+   <!-- col -->
+   <!-- col -->
+
+
+  </div>
+
+  <!-- row -->
+
+    </div>
+    <!-- footerbody -->
+   </div>
+
+  </footer>
+  <!-- footer -->
+
+
 
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"></script>
